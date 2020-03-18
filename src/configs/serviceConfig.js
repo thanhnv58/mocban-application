0ee02d1f3a6308835ec161190a1b05019a1ced0e
DESKTOP-JPJ3JUS\Thanh <thanhnv31@fsoft.com.vn>
2020-03-18 16:10:39 +0700

    Change https

--- src/configs/serviceConfig.js
+++ src/configs/serviceConfig.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:8090";
-export const BASE_URL = "https://35.223.171.192:8090";
+// export const BASE_URL = "http://35.223.171.192:8090";
+export const BASE_URL = "https://backend.mocproject.com";
